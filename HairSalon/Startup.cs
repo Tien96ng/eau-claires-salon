a8fe306a610fed28e63b21f7a5a279114880b422
tien96ng <tien96ng@ghmail.com>
2021-03-12 13:07:47 -0800

    Change Namespace and files to reference HairSalon instead of EauClairesSalon.

--- HairSalon/Startup.cs
+++ HairSalon/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using EauClaresSalon.Models;
+using HairSalon.Models;
 
-namespace EauClaresSalon
+namespace HairSalon
 {
   public class Startup
   {
@@ -25,7 +25,7 @@ namespace EauClaresSalon
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<EauClaresSalonContext>(options => options
+        .AddDbContext<HairSalonContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
